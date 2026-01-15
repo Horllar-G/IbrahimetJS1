17ca2969fed0a22dfe8ade8732ed542b017ff0a0
Horllar G <orreente06@gmail.com>
2026-01-15 12:32:53 +0100

    Add intro.js - JavaScript assignment logging name, age and favorite programming languages

---
+++ intro.js
@@ -0,0 +1,15 @@
+const name= "IBRAHIM OLAITAN YUSUF,"
+      age= "29years old"
+      ;
+
+console.log("Hello, Dear", name, age);
+
+const listlang= "html, css, JavaScript etc.";
+console.log(listlang);
+
+
+
+
+
+
+    
